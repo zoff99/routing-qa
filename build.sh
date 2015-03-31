6dd191c6beeb4cbe72aa98758c93bb145e770b81
Pierre GRANDIN <grandinp@altern.org>
2015-03-31 12:52:08 -0700

    No need to build the sample map

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/navit-gps/navit.git
 mkdir bin
-cmake ../ -Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE
+cmake ../ -Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSAMPLE_MAP:BOOL=FALSE
 make -j32
