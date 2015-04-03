c4934f5986f11d06c75e2a95d7cf162ce41ac63b
Pierre GRANDIN <grandinp@altern.org>
2015-04-03 11:31:18 -0700

    Removed build.sh, everything is in circle.yml

--- build.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/navit-gps/navit.git
-pushd
-git checkout $CIRCLE_BRANCH
-popd
-mkdir bin
-cmake ../ -Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSAMPLE_MAP:BOOL=FALSE
-make -j32
