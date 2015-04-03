d481763655f83a069d0507415174f65a8d422ef0
Pierre GRANDIN <grandinp@altern.org>
2015-04-03 11:23:59 -0700

    Test the relevant branch instead of always "master"

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/navit-gps/navit.git
+pushd
+git checkout $CIRCLE_BRANCH
+popd
 mkdir bin
 cmake ../ -Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSAMPLE_MAP:BOOL=FALSE
 make -j32
