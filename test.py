5159810d989883b4e72f2a11223b9f5a234fe3d9
jandegr <jandegr@users.noreply.github.com>
2015-11-23 19:32:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ for filename in glob.glob('*.yaml'):
         # FIXME : we should listen to a dbus signal notifying that the routing is complete instead
         timeout=50
         status=-1
-        while timeout>0 and ( status!=33 and status!=17):
+        while timeout>0 and ( status!=33 ):
             try:
                status=route.get_attr("route_status")[1]
                distance=route.get_attr("destination_length")[1]
